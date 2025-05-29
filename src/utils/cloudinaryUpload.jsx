b595bb9b7ac2dd79f4eda1e6c97c17d651cedc32
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-05-29 14:16:06 +0530

    multi image and cloudnary

--- src/utils/cloudinaryUpload.jsx
+++ src/utils/cloudinaryUpload.jsx
@@ -4,37 +4,22 @@ import imageCompression from 'browser-image-compression';
 
 const uploadToCloudinary = async (file, setUploadProgress, maxSizeKB = 800) => {
   try {
-    // Convert max size from KB to MB
     const maxSizeMB = maxSizeKB / 1024;
-
-    // Options for image compression
     const options = {
       maxSizeMB: maxSizeMB,
-      // maxWidthOrHeight: 800,
       useWebWorker: true,
     };
 
-    // Compress the image
     const compressedFile = await imageCompression(file, options);
 
-    // Check if the compressed file size is within the desired range
-    // if (compressedFile.size / 1024 > maxSizeKB) {
-    //   throw new Error(`Unable to compress image to desired size. Current size: ${(compressedFile.size / 1024).toFixed(2)} KB`);
-    // }
-
-    // Prepare form data for upload
     const data = new FormData();
     data.append("file", compressedFile);
-    // data.append("upload_preset", "whxam1fl");
-    data.append("upload_preset", "Saburi");
+    data.append("upload_preset", "grzbbapu");
     data.append("folder", "Saburi");
 
-    // email - interestingdriscoll3@typingsquirrel.com
 
-    // Upload the compressed image to Cloudinary
     const response = await axios.post(
-      // `https://api.cloudinary.com/v1_1/dzvsrft15/image/upload`,
-      `https://api.cloudinary.com/v1_1/deyyovhtb/image/upload`,
+      `https://api.cloudinary.com/v1_1/dh74mrqe0/upload`,
       data,
       {
         onUploadProgress: (progressEvent) => {
@@ -44,7 +29,7 @@ const uploadToCloudinary = async (file, setUploadProgress, maxSizeKB = 800) => {
           }
         }
       }
-    );
+    ); 
 
     if (response.status === 200) {
       return response.data.url;
